b42b7ef12ecf63f4634fc3eea98328a9f5297f30
JC Tapias <jctblock@gmail.com>
2022-08-29 15:31:56 -0400

    Checkpoint

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ canvas.height = 600;
 const background = new Image();
 background.src = "images/space.png";
 
+const enemyController = new EnemyController(canvas);
+
 function game(){
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    enemyController.draw(ctx);
 }
 
 setInterval(game, 1000/60);
