206f5f4ce1f02c2731d5b692dacb3dc7560683f7
JC Tapias <jctblock@gmail.com>
2022-08-30 12:29:24 -0400

    Checkpoint

--- Player.js
+++ Player.js
@@ -1,5 +1,16 @@
 export default class Player{
+
     constructor(canvas, velocity){
-        
+
+        this.canvas = canvas;
+        this.velocity = velocity;
+
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height - 75;
+
+        this.width = 50;
+        this.height = 48;
+
     }
+    
 }
