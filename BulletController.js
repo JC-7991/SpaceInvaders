04f8f07e11ecd7da3c47fb7c31786a6afc3341e5
JC Tapias <jctblock@gmail.com>
2022-08-31 14:03:38 -0400

    Implementing bullet mechanic

--- BulletController.js
+++ BulletController.js
@@ -19,8 +19,10 @@ export default class BulletController{
 
     draw(ctx){
 
+        this.bullets = this.bullets.filter(bullet => bullet.y + bullet.width > 0);
+
         this.bullets.forEach((bullet) => bullet.draw(ctx));
-        
+
         if(this.timeUntilNextBulletAllowed > 0){
             this.timeUntilNextBulletAllowed--;
         }
