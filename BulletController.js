70f9bca885a2cbd03b343c93ea43ef44fe94e344
JC Tapias <jctblock@gmail.com>
2022-08-31 11:10:39 -0400

    Starting bullet controls

--- BulletController.js
+++ BulletController.js
@@ -1,3 +1,24 @@
 export default class BulletController{
-    
+
+    bullets = [];
+    timeUntilNextBulletAllowed = 0;
+
+    constructor(canvas, maxBulletsAtATime, bulletColor, soundEnabled){
+
+        this.canvas = canvas;
+        this.maxBulletsAtATime = maxBulletsAtATime;
+        this.bulletColor = bulletColor;
+        this.soundEnabled = soundEnabled;
+
+        this.shootSound = new Audio("sounds/shoot.wav");
+        this.shootSound.volume = 0.5;
+
+    }
+
+    shoot(x, y, velocity, timeUntilNextBulletAllowed = 0){
+        if(this.timeUntilNextBulletAllowed <= 0 && this.bullets.length < this.maxBulletsAtATime){
+            
+        }
+    }
+
 }
